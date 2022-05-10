6e7cf550ace9c4fcf8aaaee34b616d636967c6ec
DucNguyenND <103015569+DucNguyenND@users.noreply.github.com>
2022-05-10 23:14:32 +0700

    Add files via upload

--- Snt20.java
+++ Snt20.java
@@ -1,23 +1,22 @@
-import java.util.Scanner;
-
 public class Snt20 {
     public static void main(String[] args) {
         int count=0;
         int a=2;
         System.out.println("20 số nguyên tố đầu tiên là:");
         while (count<20){
-            boolean check=true;
-            for (int i = 2; i <a ; i++) {
-                if (a%i==0) {
-                    check = false;
-                    break;
-                }
-                }
-            if (check) {
+            if (checkSNT(a)) {
                 System.out.print(a+" ");
                 count++;
             }
             a++;
-            }
         }
     }
+    public static boolean checkSNT(int a) {
+        for (int i = 2; i <a ; i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
