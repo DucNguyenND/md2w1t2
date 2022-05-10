1b9ef3fe901105ae24e256413b6e32466537559d
DucNguyenND <103015569+DucNguyenND@users.noreply.github.com>
2022-05-10 23:03:24 +0700

    Add files via upload

--- Snt100.java
+++ Snt100.java
@@ -1,19 +1,18 @@
 public class Snt100 {
     public static void main(String[] args) {
-        int count=0;
+        int count = 0;
         System.out.println("Số nguyên tố nhỏ hơn 100 là:");
-        for (int a = 2; a <100 ; a++) {
-            boolean check=true;
-            for (int i = 2; i <a ; i++) {
-                if (a%i==0) {
-                    check = false;
-                    break;
-                }
-                }
-            if (check) {
-                System.out.print(a+" ");
-                count++;
+        for (int a = 2; a < 100; a++) {
+            if (checkSNT(a)) {
+                System.out.print(a + " ");
             }
+        }
+    }
+
+        public static boolean checkSNT(int a) {
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0) return false;
             }
+            return true;
         }
     }
