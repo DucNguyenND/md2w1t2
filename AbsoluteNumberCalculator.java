568ac9448cd8f4f7b859e67dd11e18a58e9a45e6
DucNguyenND <103015569+DucNguyenND@users.noreply.github.com>
2022-05-19 11:27:46 +0700

    Add files via upload

---
+++ AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package t5w2;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
