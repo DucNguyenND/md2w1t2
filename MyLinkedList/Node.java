a6814c4d90891f9349a89a36294a962882aa4ac7
DucNguyenND <103015569+DucNguyenND@users.noreply.github.com>
2022-05-20 11:46:36 +0700

    Add files via upload

---
+++ MyLinkedList/Node.java
@@ -0,0 +1,16 @@
+package MyLinkedList;
+
+public class Node {
+    Node next;
+    private Object data;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    public Object getData() {
+        return this.data;
+    }
+
+}
+
